34e3b53de29588e823a8c08adb71f04d90d80cfb
butta <butta@KALYAN>
2024-02-08 15:28:56 +0530

    First commit

---
+++ corejavaprogram/src/corejavaprogram/program1.java
@@ -0,0 +1,11 @@
+package corejavaprogram;
+
+public class program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hema");
+System.out.println("pranitha");
+	}
+
+}
